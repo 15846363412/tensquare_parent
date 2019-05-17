c878a528e6c969e0c3ebadb1668451e540a4d208
Administrator <923282925@qq.com>
2019-05-17 09:48:30 +0800

    tensquare第二次提交

--- tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -2,17 +2,20 @@ package com.tensquare.base.controller;
 
 import com.tensquare.base.pojo.Label;
 import com.tensquare.base.service.LabelService;
+import entity.PageResult;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @RestController
 @CrossOrigin
 @RequestMapping("/label")
-public class BaseController {
+public class LabelController {
     @Autowired
     private LabelService labelService;
 
@@ -43,6 +46,17 @@ public class BaseController {
     public Result deleteById(@PathVariable("labelId") String labelId) {
         labelService.deteleById(labelId);
         return new Result(true, StatusCode.OK, "删除成功");
+    }
+
+    @RequestMapping(value = "/search" ,method = RequestMethod.POST)
+    public  Result findSearch(@RequestBody Label label){
+       List<Label> list = labelService.findSearch(label);
+       return new Result(true,StatusCode.OK,"查询成功",list);
+    }
+    @RequestMapping(value = "/{search}/{page}/{size}",method = RequestMethod.POST)
+    public Result pageQuery(@RequestBody Label label,@PathVariable int page, @PathVariable int size){
+        Page<Label> pageDate =  labelService.pageQuery(label,page,size);
+        return new Result(true,StatusCode.OK,"查询成功",new PageResult<Label>(pageDate.getTotalElements(),pageDate.getContent()));
     }
 
 }
--- tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -3,11 +3,21 @@ package com.tensquare.base.service;
 import com.tensquare.base.dao.LabelDao;
 import com.tensquare.base.pojo.Label;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.jta.WebSphereUowTransactionManager;
 import sun.reflect.generics.tree.VoidDescriptor;
 import util.IdWorker;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -36,4 +46,46 @@ public class LabelService {
         labelDao.deleteById(id);
     }
 
+    public List<Label> findSearch(Label label) {
+         return labelDao.findAll(new Specification<Label>() {
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                ArrayList<Predicate> list = new ArrayList<>();
+                if (label.getLabelname()!=null && !"".equals(label.getLabelname())) {
+                    Predicate predicate = cb.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");
+                    list.add(predicate);
+                }
+                if (label.getState()!=null && !"".equals(label.getState())){
+                    Predicate predicate = cb.like(root.get("state").as(String.class),  label.getState() );
+                    list.add(predicate);
+                }
+                Predicate[] parr = new Predicate[list.size()];
+                parr = list.toArray(parr);
+                return cb.and(parr);
+            }
+
+        });
+    }
+
+    public Page<Label> pageQuery(Label label, int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        return labelDao.findAll(new Specification<Label>() {
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                ArrayList<Predicate> list = new ArrayList<>();
+                if (label.getLabelname()!=null && !"".equals(label.getLabelname())) {
+                    Predicate predicate = cb.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");
+                    list.add(predicate);
+                }
+                if (label.getState()!=null && !"".equals(label.getState())){
+                    Predicate predicate = cb.like(root.get("state").as(String.class),  label.getState() );
+                    list.add(predicate);
+                }
+                Predicate[] parr = new Predicate[list.size()];
+                parr = list.toArray(parr);
+                return cb.and(parr);
+            }
+
+        },pageable);
+    }
 }
